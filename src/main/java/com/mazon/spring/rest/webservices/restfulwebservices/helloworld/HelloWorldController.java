9f2e404cb651e5593dfb60c264dafb8ab00a8489
Moacir Mazon <moacir.mazon@gmail.com>
2018-07-05 10:38:34 -0300

    internacionalization

--- src/main/java/com/mazon/spring/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/mazon/spring/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.mazon.spring.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping(path="/hello-world")
 	public String heloWorld(){
@@ -20,5 +26,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean heloWorldPathVariable(@PathVariable String name){
 		return new HelloWorldBean(String.format("Hello World Bean, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String heloWorldInternationalized(){
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
