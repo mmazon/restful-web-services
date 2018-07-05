797869362329f2800f20f5aabb67f77a02d3ddbd
Moacir Mazon <moacir.mazon@gmail.com>
2018-07-05 13:46:54 -0300

    adicionando swagger

--- src/main/java/com/mazon/spring/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/mazon/spring/rest/webservices/restfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="Usuarios da API")
 public class User {
 
 	private Integer id;
 	
 	@Size(min=2)
+	@ApiModelProperty(notes="Should be atleast 2 caracter")
 	private String name;
 	
 	@Past()
+	@ApiModelProperty(notes="Birth date should be before current time")
 	private Date birthDate;
 	
 	public User(Integer id, String name, Date birthDate) {
